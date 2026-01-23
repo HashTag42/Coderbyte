98bb20386acf7be7cdc3e69aa8c54e298edca23a
César García <ceegee@outlook.com>
2026-01-22 17:08:42 -0800

    Extract the test cases into a single JSON file

--- Codeland Username Validation/cs/MyTests/CodelandUsernameValidationTests.cs
+++ Codeland Username Validation/cs/MyTests/CodelandUsernameValidationTests.cs
@@ -1,19 +1,43 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
 using Xunit;
 
 namespace Coderbyte;
 
 public class CodelandUsernameValidationTests
 {
-    public static IEnumerable<object[]> TestCases =>
-        [
-            ["aa_", "false"],
-            ["u__hello_world123", "true"],
-            ["aaaaaaaaaa", "true"],
-            ["1aaaaaaaaaa", "false"],
-            ["aaaaa*aaaaa", "false"],
-            ["aaaaaaaaaa_", "false"],
-        ];
+    private static string GetTestCasesPath()
+    {
+        // Start from the compiled test assembly:
+        // .../cs/MyTests/bin/Debug/net8.0/
+        var assemblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+        // Walk up 5 levels to reach the "Coderland Username Validation" folder
+        var dir = new DirectoryInfo(assemblyDir);
+        for (int i = 0; i < 5; i++)
+            dir = dir.Parent!;
+
+        // Combine with the JSON filename
+        return Path.Combine(dir.FullName, "test_cases.json");
+    }
+
+    public static IEnumerable<object[]> TestCases
+    {
+        get
+        {
+            var jsonPath = GetTestCasesPath();
+            var json = File.ReadAllText(jsonPath);
+
+            // Deserialize into List<string[]>
+            var data = JsonSerializer.Deserialize<List<string[]>>(json)!;
+
+            // Convert to IEnumerable<object[]>
+            return data.Select(arr => arr.Cast<object>().ToArray());
+        }
+    }
 
     [Theory]
     [MemberData(nameof(TestCases))]
--- Codeland Username Validation/py/test_codeland_username_validation.py
+++ Codeland Username Validation/py/test_codeland_username_validation.py
@@ -1,18 +1,15 @@
-from codeland_username_validation import CodelandUsernameValidation
+import json
+import pathlib
 import pytest
+from codeland_username_validation import CodelandUsernameValidation
+
+root = pathlib.Path(__file__).resolve().parents[1]
+test_cases_path = root / "test_cases.json"
+
+with open(test_cases_path) as f:
+    test_cases = json.load(f)
 
 
-test_cases = [
-    # (strParam, expected)
-    ('aa_', 'false'),
-    ('u__hello_world123', 'true'),
-    ('aaaaaaaaaa', 'true'),
-    ('1aaaaaaaaaa', 'false'),
-    ('aaaaa*aaaaa', 'false'),
-    ('aaaaaaaaaa_', 'false'),
-]
-
-
-@pytest.mark.parametrize('strParam, expected', test_cases)
+@pytest.mark.parametrize("strParam, expected", test_cases)
 def test_codeland_username_validation(strParam, expected):
     assert CodelandUsernameValidation(strParam) == expected
