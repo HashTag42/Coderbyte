f612401165b52cf7c8044d68d077bf3e42f9d25a
César García <ceegee@outlook.com>
2026-01-25 19:33:23 -0800

    Move test data to separate JSON file

--- Longest Word/py/test_longest_word.py
+++ Longest Word/py/test_longest_word.py
@@ -1,12 +1,13 @@
-from longest_word import LongestWord
+import json
+import pathlib
 import pytest
+from longest_word import LongestWord
 
+root = pathlib.Path(__file__).resolve().parents[1]
+test_cases_path = root / "test_cases.json"
 
-test_cases = [
-    # (sen, expected)
-    ('fun&!! time', 'time'),
-    ('I love dogs', 'love'),
-]
+with open(test_cases_path) as f:
+    test_cases = json.load(f)
 
 
 @pytest.mark.parametrize('sen, expected', test_cases)
